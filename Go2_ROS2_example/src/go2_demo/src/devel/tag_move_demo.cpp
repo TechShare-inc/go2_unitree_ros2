96e357050d07956f6a037721293ffe767ea895cf
KojiIshikaw <koji.ishikawa@techshare.co.jp>
2024-03-01 18:31:07 +0800

    made simple-tag-demo-loop

--- Go2_ROS2_example/src/go2_demo/src/devel/tag_move_demo.cpp
+++ Go2_ROS2_example/src/go2_demo/src/devel/tag_move_demo.cpp
@@ -4,24 +4,17 @@
 #include <thread>
 #include <chrono>
 
-// タグ検出の処理を行う関数
-std::shared_ptr<apriltag_service::srv::DetectApriltag::Response> detect_apriltag(
-    const rclcpp::Node::SharedPtr& node,
-    const rclcpp::Client<apriltag_service::srv::DetectApriltag>::SharedPtr& client) {
+// タグ検出の処理を行う関数（spinが回っている前提）
+std::shared_ptr<apriltag_service::srv::DetectApriltag::Response> detect_apriltag(const rclcpp::Client<apriltag_service::srv::DetectApriltag>::SharedPtr& client) {
     auto request = std::make_shared<apriltag_service::srv::DetectApriltag::Request>();
     request->tag_size = 0.162; // 例: 16.2cmのタグサイズ
 
     auto future_result = client->async_send_request(request);
     
-    if (rclcpp::spin_until_future_complete(node, future_result) == rclcpp::FutureReturnCode::SUCCESS) {
-        return future_result.get();
-    } else {
-        RCLCPP_ERROR(node->get_logger(), "Service call failed.");
-        return nullptr;
-    }
+    return future_result.get();
 }
 
-// 回転の処理を行う関数
+// 回転の処理を行う関数（spinが回っている前提）
 void perform_rotation(const std::shared_ptr<sport_rotation>& rotation) {
     rotation->Start();
     rotation->WaitForRotationToComplete();
@@ -41,26 +34,32 @@ int main(int argc, char **argv) {
         RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "サービスが利用可能になるのを待っています...");
     }
 
-    // service call
-    auto response = detect_apriltag(node, client);
-    if (response) {
-        if (response->result == 0) {
-            RCLCPP_INFO(node->get_logger(), "成功: AprilTag: ID %ld at [x: %f, y: %f, z: %f, rotation: %f]",
-                        response->apriltag_id, response->x, response->y, response->z, response->rotation);
-        } else if (response->result == 1) {
-            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "検出失敗");
-        } else if (response->result == 99) {
-            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "カメラエラー");
-        }
-    }
-
     // go2旋回
     auto rotation = std::make_shared<sport_rotation>(node);
     // spinを別スレッドで実行
     std::thread spin_thread([&]() { rclcpp::spin(node); });
 
-    if(response && response->apriltag_id == 303){
-        perform_rotation(rotation);
+    while(true)
+    {
+        // service call
+        auto response = detect_apriltag(client);
+        if (response) {
+            if (response->result == 0) {
+                RCLCPP_INFO(node->get_logger(), "成功: AprilTag: ID %ld at [x: %f, y: %f, z: %f, rotation: %f]",
+                            response->apriltag_id, response->x, response->y, response->z, response->rotation);
+            } else if (response->result == 1) {
+                RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "検出失敗");
+            } else if (response->result == 99) {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "カメラエラー");
+            }
+        }
+
+        // rotate
+        if(response && response->apriltag_id == 303){
+            perform_rotation(rotation);
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     // spinスレッドが終了するのを待つ
